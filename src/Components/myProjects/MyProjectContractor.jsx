f50e1ad81c923dc8018ca9f8e893572b25a26799
Abhinav Sudhakar <28253243+AbhinavSdr@users.noreply.github.com>
2025-02-02 17:19:44 +0530

    update

---
+++ src/Components/myProjects/MyProjectContractor.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./MyProjectContractor.css";
+
+const MyProjectContractor = () => {
+    //data variables
+
+    const navigate = useNavigate();
+
+    // useEffect(()=>{
+    //     fetch("http:")//get user_id using username from sessionStorage
+    //     //get project_details by id
+    // })
+
+    return (
+        <div className="projects-contractor">
+            <h1 className="projects-title">Your Projects</h1>
+            {/* <div className="templates-grid">
+                {templates.map((template) => (
+                    <div key={template.id} className="template-card">
+                        <div className="template-image">
+                            <img src={template.image} alt={template.name} />
+                        </div>
+                        <div className="template-content">
+                            <h2>{template.name}</h2>
+                            <p className="price">
+                                ₹{template.pricePerSqFt}/sq.ft
+                            </p>
+                            <p className="description">
+                                {template.description}
+                            </p>
+                            <button
+                                className="select-btn"
+                                onClick={() => handleSelect(template)}
+                            >
+                                Select Design
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div> */}
+        </div>
+    );
+};
+
+export default MyProjectContractor;
